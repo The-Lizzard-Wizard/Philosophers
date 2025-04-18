bf6dc21c67e61e60cca2560f39d419f73d488020
Gael Chauvet <gchauvet@z4r11p4.42lyon.fr>
2025-04-18 17:16:21 +0200

    del .o

---
+++ src/philosophers.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gchauvet <gchauvet@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/18 15:42:07 by gchauvet          #+#    #+#             */
+/*   Updated: 2025/04/18 15:51:23 by gchauvet         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "../libft/libft.h"
+
+int	main(int argc, char **argv)
+{
+	(void)argc;
+	(void)argv;
+	ft_printf("philosophers\n");
+	return (EXIT_SUCCESS);
+}
